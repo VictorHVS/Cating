93b4fc8e7c4bb3123d6f9223c6ac2d09d76ae29b
Lucas Nogueira <lucasnogueira@outlook.com.br>
2014-05-22 12:50:36 -0300

    UnityProject Atualização de Arquivos

---
+++ UnityProject/Assets/Scripts/PlayerBehaviour/CollisonBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisonBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider hit){
+		if(hit.transform.tag == "Portal"){
+			Application.LoadLevel ("Level1");
+			//print ("respawn Level");
+			//Destroy(hit.gameObject);
+		}
+	}
+}
